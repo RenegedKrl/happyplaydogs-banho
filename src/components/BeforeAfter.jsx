d0b8c7cf0fd7db633566e9fefb6227a1568ace0d
Pinas <kaka.kfd63@gmail.com>
2026-07-10 17:57:52 -0300

    Atualizar imagens de antes e depois

--- src/components/BeforeAfter.jsx
+++ src/components/BeforeAfter.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './BeforeAfter.css';
-import beforeImg from '../assets/gallery/ANTES.jpeg';
-import afterImg from '../assets/gallery/DEPOIS.jpeg';
+import beforeImg from '../assets/gallery/antes 1.png';
+import afterImg from '../assets/gallery/depois 2.jpg';
 
 const BeforeAfter = () => {
   const [sliderPosition, setSliderPosition] = useState(50);
